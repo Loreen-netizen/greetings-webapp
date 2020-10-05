6a8bd2a6cd5dc2b59e9f103eb13e8995fc5cec5c
Loreen <loreenmuusha.com>
2020-10-05 10:36:07 +0200

    adding async await to tests

--- test/greetings.tests.js
+++ test/greetings.tests.js
@@ -1,31 +1,37 @@
 let greetingsFactoryFunction = require("../greetingsFactoryFunction.js");
 let assert = require("assert");
+let pg = require("pg");
+let Pool = pg.Pool;
+let connectionString = process.env.DATABASE_URL || 'postgresql://loreen:pg123@localhost:5432/projects';
+let pool = new Pool({
+    connectionString
+});
 describe("greetingsFactoryFunction", function() {
 
 
-    it("should greet people in 3 different languages", function() {
+    it("should greet people in 3 different languages", async function() {
 
         let greetFactoryFunction2 = greetingsFactoryFunction();
 
-        assert.equal("Hesi Kani Yeu!", greetFactoryFunction2.greetLanguage("Yeu", "Shona"));
-        assert.equal("Hello Hlosani!", greetFactoryFunction2.greetLanguage("Hlosani", "English"));
-        assert.equal("Sawubona Joey!", greetFactoryFunction2.greetLanguage("Joey", "Ndebele"))
+        assert.equal("Hesi Kani Yeu!", await greetFactoryFunction2.greetLanguage("Yeu", "Shona"));
+        assert.equal("Hello Hlosani!", await greetFactoryFunction2.greetLanguage("Hlosani", "English"));
+        assert.equal("Sawubona Joey!", await greetFactoryFunction2.greetLanguage("Joey", "Ndebele"))
     });
 
-    it("should return 1 when one person is greeted", function() {
+    it("should return 1 when one person is greeted", async function() {
 
         let greetFactoryFunction1 = greetingsFactoryFunction();
         let peopleGreeted = greetFactoryFunction1.verifyNames("Loreen")
-        assert.equal(1, greetFactoryFunction1.numberOfPeopleGreeted(peopleGreeted))
+        assert.equal(1, await greetFactoryFunction1.numberOfPeopleGreeted(peopleGreeted))
     });
 
 
 
-    it("should push a new username into an array", function() {
+    it("should push a new username into an array", async function() {
 
         let greetFactoryFunction = greetingsFactoryFunction();
         greetFactoryFunction.verifyNames("Titi");
-        assert.deepEqual(["Titi"], greetFactoryFunction.allNamesArray());
+        assert.deepEqual(["Titi"], await greetFactoryFunction.allNamesArray());
     });
 
     // it("should return error message if language is not selected", function () {
@@ -35,26 +41,27 @@ describe("greetingsFactoryFunction", function() {
     // });
 
 
-    it("should return an array with all usernames", function() {
+    it("should return an array with all usernames", async function() {
         let greetFactoryFunction5 = greetingsFactoryFunction();
-        greetFactoryFunction5.verifyNames('');
-        assert.deepEqual([], greetFactoryFunction5.allNamesArray())
+        await greetFactoryFunction5.verifyNames('');
+        assert.deepEqual([], await greetFactoryFunction5.allNamesArray())
 
 
     });
 
-    it("should be able to return number of people greeted", function() {
-        let greetFactoryFunction6 = greetingsFactoryFunction();
-        assert.equal(0, greetFactoryFunction6.numberOfPeopleGreeted())
+    it("should be able to return number of people greeted", aync
+        function() {
+            let greetFactoryFunction6 = greetingsFactoryFunction();
+            assert.equal(0, await greetFactoryFunction6.numberOfPeopleGreeted())
 
 
-    });
+        });
 
 
-    it("should return error message when user name has not been entered", function() {
+    it("should return error message when user name has not been entered", async function() {
         let greetFactoryFunction7 = greetingsFactoryFunction();
 
-        assert.equal("Please enter userName", greetFactoryFunction7.errorMessageUserName(""))
+        assert.equal("Please enter userName", await greetFactoryFunction7.errorMessageUserName(""))
 
 
     });
